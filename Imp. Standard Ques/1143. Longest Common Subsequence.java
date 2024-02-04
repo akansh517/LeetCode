907150780df054f8eb9d79b7acb38bd37d5e0b9c
Akansh Jain <90823340+akansh517@users.noreply.github.com>
2024-02-04 23:32:36 +0530

    lc

--- Imp. Standard Ques/1143. Longest Common Subsequence.java
+++ Imp. Standard Ques/1143. Longest Common Subsequence.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
+    public int longestCommonSubsequence(String text1, String text2) { 
         int m=text1.length();
         int n=text2.length();
         int dp[][]=new int[m+1][n+1];
