387296a7694057cbcddcc4c036871312c58e0d3b
unknown <akanshjain517@gmail.com>
2024-04-25 00:36:08 +0530

    lc

--- POTD/DP/1137. N-th Tribonacci Number.java
+++ POTD/DP/1137. N-th Tribonacci Number.java
@@ -1,4 +1,25 @@
 class Solution {
+    public int tribonacci(int n) {
+        if(n==0)    return 0;
+        if(n==1 || n==2)    return 1;
+        int a=0;
+        int b=1;
+        int c=1;
+        int d=0;
+        for(int i=3;i<=n;i++){
+            d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
+        }
+        return d;
+    }
+}
+// T.C:O(n)
+// S.C:O(1)
+
+
+class Solution {
     int dp[];
     public int find(int n){
         if(n<=0){
