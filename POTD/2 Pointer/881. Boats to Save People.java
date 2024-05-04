aadf7b1ba80284a4c076e57aadb72e3e88903f06
unknown <akanshjain517@gmail.com>
2024-05-04 15:07:46 +0530

    lc

---
+++ POTD/2 Pointer/881. Boats to Save People.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int boatcount=0;
+        int left=0;
+        int right=people.length-1;
+        while(left<=right){
+            if(people[left]+people[right]<=limit){
+                left++;
+                right--;
+            }
+            else{
+                right--;
+            }
+            boatcount++;
+        }
+        return boatcount;
+    }
+}
+
+// T.C:O(nlogn)
+// S.C:O(1)
