767a87c519caacd46e3cb89bcf47b25b4aef39c9
2015005 <akanshjain517@gmail.com>
2024-06-01 18:59:36 +0530

    lc

---
+++ POTD/Strings/3110. Score of a String.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int n=s.length();
+        int score=0;
+        for(int i=0;i<n-1;i++){
+            score+=Math.abs(s.charAt(i)-s.charAt(i+1));
+        }
+        return score;
+    }
+}
+
+// T.C:O(n)
+// S.C:O(1)
